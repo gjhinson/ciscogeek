e0a26c8c2485b0e333880dac9452df810bdca348
gjhinson <greg.l.hinson@gmail.com>
2016-01-07 09:10:48 -0700

    script upload to ciscogeek

---
+++ script2.py
@@ -0,0 +1,8 @@
+#cisco centric scripts for parsing out config------
+#------------------------------------
+from ciscoconfparse import CiscoConfParse
+parse = CiscoConfParse('vg1.WASPOK2410.txt')
+blockhead = parse.find_blocks('voice translation')
+print (blockhead)
+#-------------------------------------
+
