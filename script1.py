4b507521a9571393bf53ae90b27db6bc0ecabf9c
Hinson <hinsong@ds.ad.adp.com>
2016-01-07 08:39:04 -0700

    update

--- script1.py
+++ script1.py
@@ -34,4 +34,5 @@ from ciscoconfparse import CiscoConfParse
 parse = CiscoConfParse('vg1.WASPOK2410.txt')
 blockhead = parse.find_blocks('voice translation')
 print (blockhead)
+#-------------------------------------
 
