9137038a23f9fe53c1e6a624cd233912bf9e6ef3
Jay Swan <jayswan@github.com>
2016-01-07 14:02:54 -0700

    add print loop

--- script2.py
+++ script2.py
@@ -3,6 +3,7 @@
 from ciscoconfparse import CiscoConfParse
 parse = CiscoConfParse('vg1.router.txt')
 blockhead = parse.find_blocks('voice translation')
-print (blockhead)
+for item in blockhead:
+    print item
 #-------------------------------------
 
