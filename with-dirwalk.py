1b6b6f6394d1abb72e02ad253268ebe008f3e418
Jay Swan <jayswan@github.com>
2016-01-07 18:36:11 -0700

    rewrite with new samples and directory walk

---
+++ with-dirwalk.py
@@ -0,0 +1,23 @@
+import os
+from ciscoconfparse import CiscoConfParse
+
+def process_file(fname):
+    with open(fname) as f:
+        print '*** %s ***' % fname
+        parse = CiscoConfParse(f)
+        blockhead = parse.find_blocks('voice translation')
+
+        for item in blockhead:
+            print item.rstrip()
+
+def main():
+    for root,dirs,files in os.walk('.'):
+        for fname in files:
+            if fname.endswith('router.txt'):
+                process_file(os.path.join(root,fname))
+        for dirname in dirs:
+            if fname.endswith('.txt'):
+                process_file(os.path.join(root,fname))
+        
+if __name__ == "__main__":
+    main()
